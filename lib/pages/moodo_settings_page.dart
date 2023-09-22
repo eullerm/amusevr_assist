619c2df287e6049e6e097ea58e2818016d6d27a2
Euller Macena <49864491+eullerm@users.noreply.github.com>
2023-09-22 20:38:43 -0300

    Correção no import da MoodoSettingsPage

--- lib/pages/moodo_settings_page.dart
+++ lib/pages/moodo_settings_page.dart
@@ -2,7 +2,7 @@ import 'package:amusevr_assist/api/moodo_api.dart';
 import 'package:amusevr_assist/models/device.dart';
 import 'package:amusevr_assist/models/user.dart';
 import 'package:amusevr_assist/pages/home_page.dart';
-import 'package:amusevr_assist/pages/settings_esp_page.dart';
+import 'package:amusevr_assist/pages/esp_settings_page.dart';
 import 'package:amusevr_assist/utils/functions.dart';
 import 'package:amusevr_assist/widgets/custom_drawer.dart';
 import 'package:flutter/material.dart';
